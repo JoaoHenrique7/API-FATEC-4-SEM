5591bc82d7286905fe7009111bb8c0d169767de0
JoaoHenrique7 <joaohtristao@gmail.com>
2023-09-22 10:23:54 -0300

    :construction: Add rota tela de Recovery

--- frontend/src/routes/public/PublicRoutes.route.tsx
+++ frontend/src/routes/public/PublicRoutes.route.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import SignIn from "../../pages/public/SignIn/SignIn";
 import AuthLayout from "../../layout/Auth/AuthLayout";
+import RecoveryPass from "../../pages/public/RecoveryPass/RecoveryPass";
 
 function PublicRoutes(): JSX.Element {
 	return (
@@ -9,6 +10,7 @@ function PublicRoutes(): JSX.Element {
 			<Route path="/" element={<h1>Home</h1>} />
 			<Route element={<AuthLayout />}>
 				<Route path="/sign-in" element={<SignIn />} />
+				<Route path="/recovery" element={<RecoveryPass />} />
 			</Route>
 		</Routes>
 	);
