30372044c1daa3f0e6549058f7d18f5beea372bb
Henrique Pinto Ferreira Neto <dev.hnrqferreira+github@gmail.com>
2023-09-24 22:44:32 -0300

    :sparkles: feat: Login method and login page finished. Added private and public page middleware

--- backend/src/repositories/implementation/AuthRepository.ts
+++ backend/src/repositories/implementation/AuthRepository.ts
@@ -1,9 +1,11 @@
+import Endereco from "../../database/models/Endereco.model";
+import TipoUsuario from "../../database/models/TipoUsuario.model";
 import Usuario from "../../database/models/Usuario.model";
 import ILoginDTO from "../../useCases/auth/login/ILoginDTO";
 import IAuthRepository from "../IAuthRepository";
 
 export default class AuthRepository implements IAuthRepository {
 	async login(credentials: ILoginDTO): Promise<Usuario | null> {
-		return Usuario.findOne({ where: { emailUsuario: credentials.email, senhaUsuario: credentials.password } });
+		return Usuario.findOne({ where: { emailUsuario: credentials.email, senhaUsuario: credentials.password }, include: [ Endereco, TipoUsuario ] });
 	};
 }
