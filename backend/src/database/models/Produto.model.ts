b5e002341fcd66e580a25366ab8bc0d11ad1223e
Camila Silveira Redondo <caredondo97@gmail.com>
2023-10-10 11:13:24 -0300

    Create new tables

---
+++ backend/src/database/models/Produto.model.ts
@@ -0,0 +1,24 @@
+import { Column, DataType, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
+import TransacaoProduto from "./TransacaoProduto.model";
+
+@Table({ tableName: "Produto", timestamps: true })
+export default class TipoUsuario extends Model {
+	@Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false })
+	id!: number;
+
+	@Column({ type: DataType.NUMBER, allowNull: false })
+	valorProduto!: number;
+
+	@Column({ type: DataType.NUMBER, allowNull: false })
+	quantidadeEstoque!: number;
+
+	@Column({ type: DataType.STRING(255), allowNull: false })
+	descricaoProduto!: string;
+
+	@Column({ type: DataType.STRING(255), allowNull: false })
+	nomeProduto!: string;
+
+	@HasMany(() => TransacaoProduto, { constraints: false, onDelete: 'cascade' })
+	transacaoProduto!: TransacaoProduto;
+
+}
---
+++ backend/src/database/models/TransacaoProduto.model.ts
@@ -0,0 +1,24 @@
+import { Column, DataType, ForeignKey, HasMany, Model, Table } from "sequelize-typescript";
+import Carteira from "./Carteira.model";
+import Produto from "./Produto.model";
+
+@Table({ tableName: "TransacaoProduto", timestamps: true })
+export default class TipoUsuario extends Model {
+	@Column({ type: DataType.INTEGER, primaryKey: true, autoIncrement: true, allowNull: false })
+	id!: number;
+
+	@Column({ type: DataType.NUMBER, allowNull: false })
+	valorTransacaoProduto!: number;
+
+	@Column({ type: DataType.DATE, allowNull: false })
+	dataTransacaoProduto!: Date;
+
+	@ForeignKey(() => Carteira)
+	@Column({ type: DataType.INTEGER, allowNull: false })
+	idComprador!: number;
+
+	@ForeignKey(() => Produto)
+	@Column({ type: DataType.INTEGER, allowNull: false })
+	idProduto!: number;
+
+}
