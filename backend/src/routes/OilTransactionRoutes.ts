efd1af80a8da9921bdd60bd2b818f651450d3a7b
YagoPSilva <yago.pereira1388@gmail.com>
2023-10-13 19:26:38 -0300

    Feat: backend get all oil transactions

--- backend/src/routes/OilTransactionRoutes.ts
+++ backend/src/routes/OilTransactionRoutes.ts
@@ -1,9 +1,13 @@
 import { Request, Response, Router } from "express";
-import { all } from "../useCases/user/all/All";
-import { create } from "../useCases/user/oilTransaction/OilTransaction";
-import IOilTransactionDTO from "../useCases/user/oilTransaction/IOilTransactionDTO";
+import { all } from "../useCases/oilTransaction/all/All";
+import { create } from "../useCases/oilTransaction/newTransaction/OilTransaction";
+import IOilTransactionDTO from "../useCases/oilTransaction/newTransaction/IOilTransactionDTO";
 
 const router = Router();
+
+router.get("/get-all", (request: Request, response: Response) => {
+	return all.all(request, response);
+});
 
 router.post("/new-transaction", (request: Request<IOilTransactionDTO>, response: Response) => {
 	return create.newTransaction(request, response);
