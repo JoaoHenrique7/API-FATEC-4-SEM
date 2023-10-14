cbc236ccffc80f84cd91ba97e17515d6b3a8711a
JoaoHenrique7 <joaohtristao@gmail.com>
2023-10-14 11:57:53 -0300

    fix: adicionando rota de createUser e arrumando campos do formulário de transação

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -4,21 +4,19 @@ import wallet from "../../assets/wallet.svg";
 import chart from "../../assets/chart.svg";
 import profile from "../../assets/profile.svg";
 import signOut from "../../assets/signOut.svg";
-import {SessionContext, SessionContextType } from "../../context/Session/SessionContext";
-
+import { SessionContext, SessionContextType } from "../../context/Session/SessionContext";
 
 function Sidebar() {
-		const { session, logout } = useContext(SessionContext) as SessionContextType;
+	const { session, logout } = useContext(SessionContext) as SessionContextType;
 
-		return (
-			<div className={styles["sidebar"]}>
-				<img src={wallet} className={styles["options"]}></img>
-				<img src={chart} className={styles["options"]}></img>
-				<img src={profile} className={styles["options"]}></img>
-				<img src={signOut} className={styles["signOut"]} onClick={logout}></img>
-			</div>
-		);
-	}
+	return (
+		<div className={styles["sidebar"]}>
+			<img src={wallet} className={styles["options"]}></img>
+			<img src={chart} className={styles["options"]}></img>
+			<img src={profile} className={styles["options"]}></img>
+			<img src={signOut} className={styles["signOut"]} onClick={logout}></img>
+		</div>
+	);
+}
 
-export default Sidebar
-
+export default Sidebar;
